b5a776623a5044953133f17087e17b85da829eba
afroz <afroz3002@gmail.com>
2023-10-24 18:41:54 +0530

    Add files via upload

---
+++ resume-template-app/data/credentials.js
@@ -0,0 +1,14 @@
+const credentials = [
+  {
+    username: "afroz",
+    password: "alam",
+  },
+  {
+    username: "admin",
+    password: "admin",
+  },
+  {
+    username: "nikita",
+    password: "sirvi",
+  },
+];
